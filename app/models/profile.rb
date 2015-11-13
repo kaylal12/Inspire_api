1e4c65af704286cc0aeca2c5ff5f845020863663
Kaylal12 <kaylalei224@gmail.com>
2015-11-12 22:19:49 -0500

    Adds seed data. Fixes profile model.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,17 +1,14 @@
 class Profile < ActiveRecord::Base
   belongs_to :user, autosave: true
 
-  has_attached_file :poster,  #Or whatever you want to call the image you're uploading.
+  has_attached_file :profile_picture,
               :styles => { :medium => "300x300>", :thumb => "100x100>" },
               :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :poster, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :profile_picture, :content_type => /\Aimage\/.*\Z/
 
-  # By default, every file uploaded will be named 'data'.
-  # This renames the file to a timestamp, preventing name collisions.
-  # This will also fix browser caching issues for updates
-  def rename_poster
-    self.poster.instance_write :file_name, "#{Time.now.to_i.to_s}.png"
+  def rename_profile_picture
+    self.profile_picture.instance_write :file_name, "#{Time.now.to_i.to_s}.png"
   end
 
-  before_post_process :rename_poster
+  before_post_process :rename_profile_picture
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-%w(and dna dan nda).each do |name|
-  email = "#{name}@#{name}.com"
-  next if User.exists? email: email
-  User.create!(email: email, password: 'abc123',
-               password_confirmation: 'abc123')
-end
+# %w(and dna dan nda).each do |name|
+#   email = "#{name}@#{name}.com"
+#   next if User.exists? email: email
+#   User.create!(email: email, password: 'abc123',
+#                password_confirmation: 'abc123')
+# end
+
+User.delete_all
+Profile.delete_all
+
+kayla = User.create!(email: 'kayla@kay.com', password: 'kayla', password_confirmation: 'kayla')
+
+kayla.create_profile(first_name: 'Kayla', last_name: 'Lei', description: 'I like to draw')
